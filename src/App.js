857c707077d1b025f0ab0aec8f9b39871201b997
drklrd <srvbhattarai@gmail.com>
2018-02-13 10:51:32 +0545

    add turf Intersect

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import _ from 'underscore';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 import Extract from 'geojson-extract-geometries';
+import Intersect from '@turf/intersect';
 
 import NepalAdmin from './data/admin-nepal.json';
 
@@ -28,22 +29,62 @@ class App extends Component {
         selectedMunicipality: '',
     }
 
-    addGeojson = (features) => {
+    updateGeojson = (geofeatures) =>{
         if(geoJSONLayer) geoJSONLayer.clearLayers();
-        geoJSONLayer = window.L.geoJSON(features,{
+        geoJSONLayer = window.L.geoJSON(geofeatures,{
             onEachFeature: (feature,layer)=>{
                 layer.bindPopup(feature.properties);
             },
+            style : {
+                'color': '#9b59b6'
+            }
         }).addTo(this.map);
         this.map.fitBounds(geoJSONLayer.getBounds());
     }
 
+    addGeojson = (features) => {
+        this.updateGeojson(features);
+        this.map.on(window.L.Draw.Event.CREATED,(e) => {
+            let f1 = NepalAdmin.features;
+            let f2 = [e.layer.toGeoJSON()];
+            let intersections = [];
+            for (let i = 0; i < f1.length; i++) {
+                let parcel1 = f1[i];
+                for (let j = 0; j <f2.length; j++) {
+                    let parcel2 = f2[j];
+                    try {
+                        let conflict = Intersect(parcel2, parcel1);
+                        if (conflict != null) {
+                            intersections.push(conflict);
+                        }
+                    }
+                    catch(err){
+                    }
+                }
+            }
+            this.updateGeojson(intersections);
+        });
+    }
+
     componentDidMount() {
-        const map = this.map = window.L.map(ReactDOM.findDOMNode(this.refs['map']),{editable: true}).setView([28.2380, 83.9956], 3);
+        const map = this.map = window.L.map(ReactDOM.findDOMNode(this.refs['map']),{}).setView([28.2380, 83.9956], 3);
         window.L.tileLayer.grayscale('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
         this.addGeojson(features);
+        // FeatureGroup is to store editable layers
+        var drawnItems = new window.L.FeatureGroup();
+        map.addLayer(drawnItems);
+        var drawControl = new window.L.Control.Draw({
+            draw: {
+                polygon: false,
+                marker: false,
+                circle : false,
+                polyline : false
+            }
+        });
+        map.addControl(drawControl);
+
     }
 
     handleProvinceChange = (selected) => {
@@ -133,70 +174,69 @@ class App extends Component {
         const valueMunicipality = selectedMunicipality && selectedMunicipality.value;
         return (
             <div>
-                <Draggable>
-                    <div className="select-file">
-                        <div className="row">
-                            <h4> Download admin boundaries for Nepal</h4>
+                <div className="select-file">
+                    <div className="row">
+                        <h5> Download admin boundaries for Nepal</h5>
+                    </div>
+                    <div className="row">
+                        <div className="col-4">
+                            <Select
+                                className="select"
+                                placeholder = 'Select Province'
+                                value={valueProvince}
+                                onChange={this.handleProvinceChange}
+                                options={provinces.map((province)=>{
+                                    return {
+                                        value : province,
+                                        label : province,
+                                    };
+                                })}
+                            />
                         </div>
-                        <div className="row">
-                            <div className="col-4">
-                                <Select
-                                    className="select"
-                                    placeholder = 'Select Province'
-                                    value={valueProvince}
-                                    onChange={this.handleProvinceChange}
-                                    options={provinces.map((province)=>{
-                                        return {
-                                            value : province,
-                                            label : province,
-                                        };
-                                    })}
-                                />
-                            </div>
-                            <div className="col-4">
-                                <Select
-                                    className="select"
-                                    placeholder = 'Select District'
-                                    value={valueDistrict}
-                                    onChange={this.handleDistrictChange}
-                                    options={districts.map((district)=>{
-                                        return {
-                                            value : district,
-                                            label : district,
-                                        };
-                                    })}
-                                />
-                            </div>
-                            <div className="col-4">
-                                <Select
-                                    className="select"
-                                    placeholder = 'Select Municipality'
-                                    value={valueMunicipality}
-                                    onChange={this.handleMunicipalityChange}
-                                    options={municipalities.map((municipality)=>{
-                                        return {
-                                            value : municipality,
-                                            label : municipality,
-                                        };
-                                    })}
-                                />
-                            </div>
+                        <div className="col-4">
+                            <Select
+                                name="form-field-name"
+                                className="select"
+                                placeholder = 'Select District'
+                                value={valueDistrict}
+                                onChange={this.handleDistrictChange}
+                                options={districts.map((district)=>{
+                                    return {
+                                        value : district,
+                                        label : district,
+                                    };
+                                })}
+                            />
                         </div>
-                        <br/>
-                        <div className="row">
-                            <div className="col-3"></div>
-                            <div className="col-2">
-                                <button className="download-geojson button-color-blue" onClick={this.downloadGeoJSON}> <i className="fa fa-download" aria-hidden="true"></i>   GeoJSON </button>
-                            </div>
-                            <div className="col-2">
-                                <button className="download-geojson button-color-blue" onClick={this.downloadTopoJSON}> <i className="fa fa-download" aria-hidden="true"></i> TopoJSON </button>
-                            </div>
-                            <div className="col-2">
-                                <button className="download-geojson button-color-blue" onClick={this.downloadPoly}> <i className="fa fa-download" aria-hidden="true"></i> Poly file </button>
-                            </div>
+                        <div className="col-4">
+                            <Select
+                                className="select"
+                                placeholder = 'Select Municipality'
+                                value={valueMunicipality}
+                                onChange={this.handleMunicipalityChange}
+                                options={municipalities.map((municipality)=>{
+                                    return {
+                                        value : municipality,
+                                        label : municipality,
+                                    };
+                                })}
+                            />
                         </div>
                     </div>
-                </Draggable>
+                    <br/>
+                    <div className="row">
+                        <div className="col-3"></div>
+                        <div className="col-2">
+                            <button className="download-geojson button-color-blue" onClick={this.downloadGeoJSON}> <i className="fa fa-download" aria-hidden="true"></i>   GeoJSON </button>
+                        </div>
+                        <div className="col-2">
+                            <button className="download-geojson button-color-blue" onClick={this.downloadTopoJSON}> <i className="fa fa-download" aria-hidden="true"></i> TopoJSON </button>
+                        </div>
+                        <div className="col-2">
+                            <button className="download-geojson button-color-blue" onClick={this.downloadPoly}> <i className="fa fa-download" aria-hidden="true"></i> Poly file </button>
+                        </div>
+                    </div>
+                </div>
                 <div ref={'map'} className="map"/>
             </div>
         );
